a18641b7aa35868cfac2055fac9365455cbbdd24
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-08-24 00:00:33 +0700

    feat: initialize Prisma database schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { OrganizationsModule } from './organizations/organizations.module';
-import { RefreshTokensModule } from './refresh_tokens/refresh_tokens.module';
 import { RolesModule } from './roles/roles.module';
 import { MembershipsModule } from './memberships/memberships.module';
 import { PermissionsModule } from './permissions/permissions.module';
-import { RolePermissionsModule } from './role_permissions/role_permissions.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, OrganizationsModule, RefreshTokensModule, RolesModule, MembershipsModule, PermissionsModule, RolePermissionsModule, AuthModule],
+  imports: [UsersModule, OrganizationsModule, RolesModule, MembershipsModule, PermissionsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
